361e2487a525e72c654b9ca4a0b8448d711fa5f4
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-18 11:01:33 +0700

    menambahkan autoload helper info_helper

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -27,7 +27,7 @@ class BaseController extends Controller
 	 *
 	 * @var array
 	 */
-	protected $helpers = ['html', 'form'];
+	protected $helpers = ['html', 'form', 'info'];
 
 	/**
 	 * Constructor.
