40b98f2303602d11a6e75a1adaac480c449d9abf
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-19 10:45:41 +0700

    component baru

---
+++ app/Views/layout/components/js/alert.php
@@ -0,0 +1,6 @@
+<div class="alert alert-js d-none" role="alert">
+    <span class="message"></span>
+    <button type="button" class="close font-weight-normal" aria-label="Close">
+        <span aria-hidden="true">&times;</span>
+    </button>
+</div>
---
+++ app/Views/layout/components/modal-delete.php
@@ -0,0 +1,20 @@
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="konfirmasi-hapus">
+    <div class="modal-dialog modal-sm shadow-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header m-0 py-2">
+                <h5 class="modal-title font-weight-normal">Konfirmasi Hapus</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <i class="fa fa-exclamation-triangle fa-3x text-warning mr-2 float-left"></i>
+                Apakah anda yakin ingin menghapus data ini?<br>
+            </div>
+            <div class="modal-footer py-2">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fa fa-times-circle"></i> Batal</button>
+                <button type="button" id="btn-hapus" class="btn btn-danger"><i class="far fa-trash-alt"></i> Hapus</button>
+            </div>
+        </div>
+    </div>
+</div>
