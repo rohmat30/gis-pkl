2e5b53e067d1bffd8c35ddfa41141f01296ffc1b
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-19 21:41:50 +0700

    component image preview

---
+++ app/Views/layout/components/modal-image.php
@@ -0,0 +1,20 @@
+<div class="modal fade" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel" aria-hidden="true" id="preview-gambar">
+    <div class="modal-dialog modal-sm shadow-lg" role="document">
+        <div class="modal-content">
+            <div class="modal-header m-0 py-2">
+                <h5 class="modal-title font-weight-normal">Preview Gambar</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div style="background:#eee" class="text-center">
+                    <img src="" alt="" class="mw-100">
+                </div>
+            </div>
+            <div class="modal-footer py-2">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal"><i class="fa fa-times-circle"></i> Tutup</button>
+            </div>
+        </div>
+    </div>
+</div>
