7f2c76e385608d6b99c0afad35d55ba04eb63982
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-19 21:43:59 +0700

    Menambah file migrasi pengajuan instansi

---
+++ app/Database/Migrations/2020-07-19-042222_Pengajuan_instansi.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class PengajuanInstansi extends Migration
+{
+	protected $table = 'pengajuan_instansi';
+	public function up()
+	{
+		//
+		$this->forge
+			->addPrimaryKey('pengajuan_id')
+			->addField([
+				'pengajuan_id' => [
+					'type'           => 'INT',
+					'constraint'     => 11,
+					'unsigned'       => TRUE,
+					'auto_increment' => TRUE
+				],
+				'nama' => [
+					'type'       => 'VARCHAR',
+					'constraint' => 100
+				],
+				'alamat' => [
+					'type'       => 'VARCHAR',
+					'constraint' => 255,
+					'unique'     => TRUE
+				],
+				'pic' => [
+					'type'       => 'VARCHAR',
+					'constraint' => 50
+				],
+				'foto' => [
+					'type'       => 'VARCHAR',
+					'constraint' => 255
+				],
+				'status'      => [
+					'type'       => 'ENUM',
+					'constraint' => ['setuju', 'tidak', 'menunggu']
+				],
+				'user_id' => [
+					'type'       => 'INT',
+					'constraint' => 11,
+					'unsigned'   => TRUE
+				],
+				'created_at' => [
+					'type' => 'DATETIME',
+					'null' => TRUE
+				],
+				'updated_at' => [
+					'type' => 'DATETIME',
+					'null' => TRUE
+				],
+				'deleted_at' => [
+					'type' => 'DATETIME',
+					'null' => TRUE
+				]
+			])
+			->createTable($this->table);
+	}
+
+	//--------------------------------------------------------------------
+
+	public function down()
+	{
+		//
+		$this->forge->dropTable($this->table);
+	}
+}
