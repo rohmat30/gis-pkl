f21343e939d58741027bb90d2dc3e4b48fa70b1f
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-19 10:44:10 +0700

    menambahkan copyright

--- app/Views/layout/includes/app-footer.php
+++ app/Views/layout/includes/app-footer.php
@@ -1,1 +1,6 @@
+<footer class="bg-light mt-3">
+    <div class="container-md py-3">
+        &copy; 2020 <a href="https://github.com/rohmat30">GIS Prakerin</a>
+    </div>
+</footer>
 <a href="#" id="back-to-top"><i class="fas fa-arrow-up"></i></a>
