a0658833f79432350c09aae8a07d190064cfa66d
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-19 21:40:33 +0700

    hapus bottom section

--- app/Views/pengguna/v-pengguna-form.php
+++ app/Views/pengguna/v-pengguna-form.php
@@ -42,11 +42,4 @@ echo $this->section('content');
 </div>
 <?php
 echo $this->endSection();
-
-// bottom assets
-echo $this->section('bottom_assets');
-echo script_tag('assets/lib/datatables/js/jquery.dataTables.min.js');
-echo script_tag('assets/lib/datatables-bs4/js/dataTables.bootstrap4.min.js');
-echo script_tag('assets/js/app/app.user.dt.js');
-echo $this->endSection()
 ?>
