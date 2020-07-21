eb6e1720858922b06803ab0d3eb08e808ef0817e
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-21 08:02:09 +0700

    menambah marker icon

---
+++ public/assets/js/app/app.marker.icon.js
@@ -0,0 +1,20 @@
+var redIcon = L.icon({
+  iconUrl: "/assets/img/marker-red.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [14, 41], // the same for the shadow
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
+var tealIcon = L.icon({
+  iconUrl: "/assets/img/marker-teal.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [14, 41], // the same for the shadow
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
