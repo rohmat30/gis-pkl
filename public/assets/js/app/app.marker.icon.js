327f2551dd4c13d57a9db75b206838d8a5ccaf16
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-25 15:38:53 +0700

    tambah variasi icon

--- public/assets/js/app/app.marker.icon.js
+++ public/assets/js/app/app.marker.icon.js
@@ -18,3 +18,47 @@ var tealIcon = L.icon({
   shadowAnchor: [14, 41], // the same for the shadow
   popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
 });
+
+var grayIcon = L.icon({
+  iconUrl: "/assets/img/marker-gray.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [14, 41], // the same for the shadow
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
+
+var grayIconSm = L.icon({
+  iconUrl: "/assets/img/marker-gray.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [19, 30], // size of the icon
+  shadowSize: [30, 30], // size of the shadow
+  iconAnchor: [9, 30], // point of the icon which will correspond to marker's location
+  shadowAnchor: [11, 30], // the same for the shadow
+  popupAnchor: [0, -28], // point from which the popup should open relative to the iconAnchor
+});
+
+var blueIcon = L.icon({
+  iconUrl: "/assets/img/marker-blue.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [25, 41], // size of the icon
+  shadowSize: [41, 41], // size of the shadow
+  iconAnchor: [12, 41], // point of the icon which will correspond to marker's location
+  shadowAnchor: [14, 41], // the same for the shadow
+  popupAnchor: [0, -38], // point from which the popup should open relative to the iconAnchor
+});
+
+var blueIconSm = L.icon({
+  iconUrl: "/assets/img/marker-blue.png",
+  shadowUrl: "/assets/lib/leaflet/images/marker-shadow.png",
+
+  iconSize: [19, 30], // size of the icon
+  shadowSize: [30, 30], // size of the shadow
+  iconAnchor: [9, 30], // point of the icon which will correspond to marker's location
+  shadowAnchor: [11, 30], // the same for the shadow
+  popupAnchor: [0, -28], // point from which the popup should open relative to the iconAnchor
+});
