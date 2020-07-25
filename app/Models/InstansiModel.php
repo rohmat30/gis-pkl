a258b0325dd08b065de09472617f47efedb1d353
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-25 15:38:37 +0700

    tambah format geojson

--- app/Models/InstansiModel.php
+++ app/Models/InstansiModel.php
@@ -46,4 +46,57 @@ class InstansiModel extends Model
 
         return $this->findAll(5);
     }
+
+
+    public function geojson()
+    {
+        $sql = $this->selectCount('jadwal.jadwal_id', 'jumlah_siswa')
+            ->select('instansi.instansi_id')
+            ->join('jadwal', 'instansi.instansi_id = jadwal.instansi_id')
+            ->groupStart()
+            ->where('tanggal_awal <=', 'DATE(NOW())', FALSE)
+            ->where('tanggal_akhir >=', 'DATE(NOW())', FALSE)
+            ->groupEnd()
+            ->where('jadwal.deleted_at IS NULL')
+            ->groupBy('instansi.instansi_id')
+            ->getCompiledSelect();
+        $sql_alias = '(' . $sql . ') as a';
+        $rows = $this->select('instansi.*,coalesce(a.jumlah_siswa, 0) as jumlah_siswa')
+            ->join($sql_alias, 'a.instansi_id = instansi.instansi_id', 'LEFT')
+            ->findAll();
+
+        $results = [
+            "type" => "FeatureCollection",
+            "name" => "Lokasi Prakerin",
+            "crs" => [
+                "type" => "name",
+                "properties" => [
+                    "name" => "urn:ogc:def:crs:OGC:1.3:CRS84"
+                ]
+            ],
+            "features" => []
+        ];
+        foreach ($rows as $key => $row) {
+            $result = [
+                "type" => "Feature",
+                "properties" => [
+                    "name" => $row->nama_instansi,
+                    "address" => $row->alamat,
+                    "status" => $row->status,
+                    "photo" => base_url(['uploads', $row->foto]),
+                    "students" => intval($row->jumlah_siswa)
+                ],
+                "geometry" => [
+                    "type" => "Point",
+                    "coordinates" => [
+                        $row->longitude + 0,
+                        $row->latitude + 0
+                    ]
+                ]
+            ];
+            $results["features"][$key] = $result;
+        }
+
+        return $results;
+    }
 }
