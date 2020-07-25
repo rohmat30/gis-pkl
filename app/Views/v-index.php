ed37db78395f951ff385531914dddbd68dad11a4
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-25 15:37:28 +0700

    Tambah content pesan selamat datang

--- app/Views/v-index.php
+++ app/Views/v-index.php
@@ -1,1 +1,8 @@
 <?= $this->extend('layout\app') ?>
+<?= $this->section('content') ?>
+<div class="alert alert-primary">
+    <p class="m-0">
+        Selamat datang di <strong><?= site()->appName ?></strong>
+    </p>
+</div>
+<?= $this->endSection() ?>
