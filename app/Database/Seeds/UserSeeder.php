5e5dc53db9889440c2374e761a92a5d2d846e6e8
Muhammad Rohmat <rohmatif30@gmail.com>
2020-07-26 08:06:42 +0700

    menambahkan kemampuan pembuatan jumlah user

--- app/Database/Seeds/UserSeeder.php
+++ app/Database/Seeds/UserSeeder.php
@@ -8,29 +8,102 @@ use Faker\Factory;
 
 class UserSeeder extends \CodeIgniter\Database\Seeder
 {
+    protected $model;
+    protected $jumlahPembimbing;
+    protected $jumlahPL;
+    protected $jumlahSiswa;
+    protected $jumlahTotal;
+    public function __construct()
+    {
+        $this->model = new UserModel;
+    }
+
     public function run()
     {
-        $model = new UserModel;
+        $roles = ['staff_tu', 'kajur'];
+        $username = ['staff', 'kajur'];
 
-        $roles = ['staff_tu', 'kajur', 'pembimbing', 'pembimbing_lapangan', 'siswa'];
-        $username = ['staff', 'kajur', 'pembimbing', 'pemlap', 'siswa'];
+        $data = $this->model->first();
+        if ($data) {
+            $replacement = CLI::prompt('Data dalam tabel sudah ada, apakah ingin menghapusnya?', ['y', 'n']);
+            if ($replacement == 'y') {
+                $this->model->truncate();
+            } else {
+                return;
+            }
+        }
+        $this->jumlahPembimbing = intval(CLI::prompt('Masukan jumlah pembimbing', '5'));
+        $this->jumlahPL = intval(CLI::prompt('Masukan jumlah pembimbing lapangan', '10'));
+        $this->jumlahSiswa = intval(CLI::prompt('Masukan jumlah siswa', '50'));
+        $this->jumlahTotal = intval(2 + $this->jumlahPembimbing + $this->jumlahPL + $this->jumlahSiswa);
 
-        $data = [];
+        $this->model->transStart();
         foreach ($roles as $key => $role) {
             $name = $this->getName();
-            $data[$key] = [
+            $data = [
                 'nama' => $name,
                 'username' => $username[$key],
                 'password' => '12345',
                 'role' => $role
             ];
-
-            $model->insert($data[$key]);
-            CLI::showProgress($key, (count($roles) - 1));
+            $this->model->insert($data);
+            CLI::showProgress(++$key, $this->jumlahTotal);
         }
+        $this->getPembimbing();
+        $this->getPL();
+        $this->getSiswa();
+        if ($this->model->transStatus() === FALSE) {
+            $this->model->transRollback();
+            CLI::write('Tidak berhasil menambahkan user!');
+        } else {
+            $this->model->transCommit();
+            CLI::showProgress(false);
+        }
+    }
 
-        CLI::showProgress(false);
-        CLI::table($data, ['nama', 'username', 'password', 'level']);
+    protected function getPembimbing()
+    {
+        foreach (range(1, $this->jumlahPembimbing) as $p) {
+            $name = $this->getName();
+            $data = [
+                'nama' => $name,
+                'username' => 'p' . str_pad($p, '2', '0', STR_PAD_LEFT),
+                'password' => '12345',
+                'role' => 'pembimbing'
+            ];
+            $this->model->insert($data);
+            CLI::showProgress(2 + $p, $this->jumlahTotal);
+        }
+    }
+
+    protected function getPL()
+    {
+        foreach (range(1, $this->jumlahPL) as $pl) {
+            $name = $this->getName();
+            $data = [
+                'nama' => $name,
+                'username' => 'pl' . str_pad($pl, '2', '0', STR_PAD_LEFT),
+                'password' => '12345',
+                'role' => 'pembimbing_lapangan'
+            ];
+            $this->model->insert($data);
+            CLI::showProgress(2 + $this->jumlahPembimbing + $pl, $this->jumlahTotal);
+        }
+    }
+
+    protected function getSiswa()
+    {
+        foreach (range(1, $this->jumlahSiswa) as $s) {
+            $name = $this->getName();
+            $data = [
+                'nama' => $name,
+                'username' => '20211' . str_pad($s, '3', '0', STR_PAD_LEFT),
+                'password' => '12345',
+                'role' => 'siswa'
+            ];
+            $this->model->insert($data);
+            CLI::showProgress(2 + $this->jumlahPembimbing + $this->jumlahPL + $s, $this->jumlahTotal);
+        }
     }
 
     private function getName()
